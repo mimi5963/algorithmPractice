8957645dc5090fca3d6ee252dbf39a6653493e60
mimi5963 <mimi5963@naver.com>
2024-05-12 23:38:01 +0900

    DFS

---
+++ src/main/java/org/example/dfs/Boj15451.java
@@ -0,0 +1,19 @@
+package org.example.dfs;
+
+import java.util.Scanner;
+
+public class Boj15451 {
+    static int n,m;
+    static boolean[] visited;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        m = sc.nextInt();
+        //
+        visited = new boolean[n];
+    }
+
+    public static void dfs(int node){
+
+    }
+}
